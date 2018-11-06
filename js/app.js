f2b6ea5a43707c4a7a57e959d189c25b368e5013
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-11-06 15:21:10 -0800

    initial commit in this repo of a file committed in a previously corrupted repo!

---
+++ js/app.js
@@ -0,0 +1,38 @@
+import pokeApi from './pokemon-api.js';
+import pokeFilter from './pokemon-filter.js';
+import pokeTable from './pokemon-table.js';
+
+const pokedex = pokeApi.getAll();
+
+pokeTable.init(pokedex);
+
+pokeFilter.init(function(nameFilter, type_1Filter, type_2Filter, ability1Filter, hpFilter) {
+    let filtered;
+
+    if(nameFilter || type_1Filter || type_2Filter || ability1Filter || hpFilter) {
+        nameFilter = nameFilter.toLowerCase();
+        type_1Filter = type_1Filter.toLowerCase();
+        type_2Filter = type_2Filter.toLowerCase();
+        ability1Filter = ability1Filter.toLowerCase();
+        //hpFilter = hpFilter.toLowerCase();
+
+        filtered = pokedex.filter(function(pokemons) {
+            const hasPokemon = !nameFilter
+            || pokemons.pokemon.toLowerCase().includes(nameFilter);
+            const hasType1 = !type_1Filter
+            || pokemons.type_1.toLowerCase().includes(type_1Filter); // fixed - changed pokemons.type_1filter to pokemons.type_1
+            const hasType2 = !type_2Filter
+            || pokemons.type_2.toLowerCase().includes(type_2Filter); // fixed! - ''
+            const hasAbility1 = !ability1Filter
+            || pokemons.ability_1.toLowerCase().includes(ability1Filter); // fixed! ''
+            const hasHp = !hpFilter
+            || pokemons.hp >= hpFilter;  // Work out how to make numeric work
+
+            return hasPokemon && hasType1 && hasType2 && hasAbility1 && hasHp;
+        });
+    }
+    else {
+        filtered = pokedex;
+    }
+    pokeTable.update(filtered);
+});
---
+++ js/pokemon-api.js
@@ -0,0 +1,9 @@
+import pokedex from './data/pokedex.js';
+
+const pokeApi = {
+    getAll() {
+        return pokedex;
+    }
+};
+
+export default pokeApi;
