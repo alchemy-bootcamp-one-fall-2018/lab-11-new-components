3a75c752332cb774414838f97d132857f115ecde
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-11-21 13:27:50 -0800

    added arrow function & truncation per new syntax

--- js/pokemon-filter.js
+++ js/pokemon-filter.js
@@ -25,3 +25,25 @@ const pokeFilter = {
 };
 
 export default pokeFilter;
+
+// const pokeFilter = {
+//     init(onFilter) {
+//         nameFilter.addEventListener('keyup', ()=> {
+//             onFilter(nameFilter.value, type_1Filter.value, type_2Filter.value, ability1Filter.value, hpFilter.value);
+//         });
+//         type_1Filter.addEventListener('keyup', ()=> {
+//             onFilter(nameFilter.value, type_1Filter.value, type_2Filter.value, ability1Filter.value, hpFilter.value);
+//         });
+//         type_2Filter.addEventListener('keyup', ()=> {
+//             onFilter(nameFilter.value, type_1Filter.value, type_2Filter.value, ability1Filter.value, hpFilter.value);
+//         });
+//         ability1Filter.addEventListener('keyup', ()=> {
+//             onFilter(nameFilter.value, type_1Filter.value, type_2Filter.value, ability1Filter.value, hpFilter.value);
+//         });
+//         hpFilter.addEventListener('keyup', ()=> {
+//             onFilter(nameFilter.value, type_1Filter.value, type_2Filter.value, ability1Filter.value, hpFilter.value);
+//         });
+//     }
+// };
+
+// export default pokeFilter;
